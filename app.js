55504979df8e43d982e9f63cbbad154c1dd9b5a4
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-01 17:44:16 +0100

    Modif

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+let pUn = document.getElementById("entier");
+let pDeux = document.getElementById("flottant");
+let pTrois = document.getElementById("non-numerique");
+let pQuatre = document.getElementById("random");
+let pCinq = document.getElementById("trunc");
+let pSix = document.getElementById("replace");
+let operande = 10
+
+pUn.innerHTML = parseInt(pUn.innerText) * operande;
+pDeux.innerHTML = parseFloat(pDeux.innerText) * operande + parseInt(pUn.innerText);
+
+if (isNaN(pTrois.innerText)) {
+    pTrois.innerHTML = "0";
+}
+
+pQuatre.innerHTML = Math.random();
+pCinq.innerHTML = Math.trunc(pCinq.innerText);
+pSix.innerHTML = pSix.innerText.replace("Hello world", "Jocaille Amaury");
+
 console.log(random());
